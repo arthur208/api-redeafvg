71ce0e9580598af1ddc72bd21c9c2caff4507ec2
arthur.zacarias <arthur_zacarias@alunos.unicesumar.edu.br>
2022-09-23 18:03:49 -0300

    adicionar cors

--- src/main/java/br/edu/unicesumar/example/controller/FornecedorController.java
+++ src/main/java/br/edu/unicesumar/example/controller/FornecedorController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +41,7 @@ public class FornecedorController {
     // public ResponseEntity<Page<Fornecedor>> buscarTodos(Pageable pageable) {
     //     return ResponseEntity.ok(this.service.findAll(pageable));
     // }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public ResponseEntity<Page<Fornecedor>> buscarTodosPorNome(@RequestParam(name = "Fornecedor", required = false, defaultValue = "") String fornecedor, Pageable pageable) {
         return ResponseEntity.ok(service.findAll(fornecedor, pageable));
