08789671a2f5a39f3acf6eaebcd0699350e3ecb6
Arthur208 <arthurduartezacarias@gmail.com>
2022-09-25 23:16:39 -0300

    cors certo

--- src/main/java/br/edu/unicesumar/example/controller/TestPermissionController.java
+++ src/main/java/br/edu/unicesumar/example/controller/TestPermissionController.java
@@ -2,6 +2,7 @@ package br.edu.unicesumar.example.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
