f493f357dcc2f6de64b0f9a5ace8e896d12625cb
arthur.zacarias <arthur_zacarias@alunos.unicesumar.edu.br>
2022-09-27 23:46:24 -0300

    correções

--- src/main/java/br/edu/unicesumar/example/controller/UsersController.java
+++ src/main/java/br/edu/unicesumar/example/controller/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import br.edu.unicesumar.example.domain.Users;
 
 @RestController
+@CrossOrigin("*") 
 @RequestMapping("/users")
 public class UsersController {
     
